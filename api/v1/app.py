c1084f64288c00d388015a18040a95be49728eac
dalejoroc11 <dalejoroc11@github.com>
2020-09-11 04:04:45 +0000

    task 13 fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""App Module
-    Return the status of the API
-"""
-from flask import Flask, jsonify
+"""Contains app-flask and endpoints (routes)"""
+from flask import Flask, Blueprint, abort
+from flask import render_template, jsonify
+from flask import make_response
+from flask_cors import CORS
+import os
 from models import storage
 from api.v1.views import app_views
-import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
-def errorhandler(error):
-    """Returns a json formatted 404 status code"""
-    return jsonify({"error": "Not found"}), 404
+def invalid_route(e):
+    return (jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove the current session"""
+def storage_close(issue):
+    """calls storage.close()"""
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', default="0.0.0.0")
-    port = int(os.getenv('HBNB_API_PORT', default=5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')),
+            threaded=True, debug=True)
